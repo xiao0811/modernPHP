e4661f387b18688f750dce25771bb3e64f4ca454
潘骁 <panxiao@panxiaodeMacBook-Pro.local>
2018-03-13 22:54:27 +0800

    autoload 1.2

--- spl_autoload_register/Os/autoload.php
+++ spl_autoload_register/Os/autoload.php
@@ -5,18 +5,21 @@
  * Date: 2018-3-13
  * Time: 15:54
  */
-
+//use Os\Linux;
 spl_autoload_register(function ($class) {
-    $class_map = [
-        'os\Linux' => './Linux.php'
-    ];
-
-    $file = $class_map[$class];
-//    echo $file;
+//    $class_map = [
+//        'os\Linux' => './Linux.php'
+//    ];
+//
+//    $file = $class_map[$class];
+    $class = str_replace('\\', '/', $class);
+    $file = "../$class.php";
+    echo $file;
 
     if (file_exists($file)) {
         require_once $file;
+        echo "has";
     }
 });
 
-new \os\Linux();
+new \Os\Linux();
