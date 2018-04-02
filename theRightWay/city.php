9d6a01a9e154310fedd05c8f842230df8366dfcc
xiao0811 <panxiao0811@gmail.com>
2018-04-02 16:54:28 +0800

    city json

---
+++ theRightWay/city.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 骁傻
+ * Date: 2018-4-2
+ * Time: 16:49
+ */
+
+$data = file_get_contents("city.json");
+echo $data;
---
+++ theRightWay/error.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 骁傻
+ * Date: 2018-4-2
+ * Time: 13:48
+ */
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+//$name = "xiaosha";
+echo isset($name) ? $name : "abc";
