63f4c4f7533394e8293bbc2e3d638e5d28971646
xiao0811 <panxiao0811@gmail.com>
2018-04-10 16:24:21 +0800

    getCode

--- Code/getCode.php
+++ Code/getCode.php
@@ -3,11 +3,9 @@
  * Created by PhpStorm.
  * User: 骁傻
  * Date: 2018-4-10
- * Time: 16:01
+ * Time: 16:21
  */
-
 require_once "Code.class.php";
-
 session_start();
 $code = new Code();
-$code->make();
+echo $code->get();
