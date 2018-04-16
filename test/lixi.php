b017ba6afa1bbaf8863bc596b732abef9475f964
xiao0811 <panxiao0811@gmail.com>
2018-04-16 09:49:35 +0800

    nothing

---
+++ test/lixi.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 骁傻
+ * Date: 2018-4-16
+ * Time: 9:21
+ */
+
+$buy_time = strtotime('2017-10-16');
+$end_time = strtotime(date("2018-4-12"));
+echo ($end_time - $buy_time) / 86400;
