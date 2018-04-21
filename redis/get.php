ec0199afcf9d0cf4466ddfa8c542515d2c958734
xiao0811 <panxiao0811@gmail.com>
2018-04-21 13:51:59 +0800

    data

--- redis/get.php
+++ redis/get.php
@@ -5,15 +5,15 @@
  * Date: 2018/4/21
  * Time: 下午1:09
  */
-/**
-$redis = new Redis();
-$redis->connect("127.0.0.1");
 
-//echo "<pre>";
-//var_dump(json_decode($redis->get('less')));
-//echo "</pre>";
-
-echo $redis->get('less');*/
+//$redis = new Redis();
+//$redis->connect("127.0.0.1");
+//
+////echo "<pre>";
+////var_dump(json_decode($redis->get('less')));
+////echo "</pre>";
+//
+//echo $redis->get('less');
 
 $pdo = new PDO('mysql:host=localhost;dbname=laravel', 'root', 'root');
 $sql = "SELECT * FROM `xiaosha` WHERE `id` = 6";
