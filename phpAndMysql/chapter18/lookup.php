a6c697b2df2bc1362f86d4feb0c665509a442c2b
xiao0811 <panxiao0811@gmail.com>
2018-05-11 17:00:01 +0800

    0511

---
+++ phpAndMysql/chapter18/lookup.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Stock Quote From NASDAQ</title>
+</head>
+<body>
+<?php
+// chose stock to look at
+$symbol = 'GOOG';
+echo '<h1>Stock Quote for' . $symbol . '</h1>';
+$url = 'http://download.finance.yahoo.com/d/quotes.csv?s=' . $symbol .
+    '&e=.csv&f=sl1d1t1c1ohgv';
+
+if (!($contents = file_get_contents($url))) {
+    die('Failed to open' . $url);
+}
+
+// extract relevant data
+list($symbol, $quote, $date, $time) = explode(',', $contents);
+$date = trim($date, '"');
+$time = trim($time, '"');
+
+echo "<p>$symool was last sold at \$$quote</p>";
+echo "<p>Quote current as of $date at $time</p>";
+
+// acknowledge source
+echo '<p>This information retrieved from<br><a href="' . $url . '"> ' . $url . '</a></p>';
+?>
+</body>
+</html>
