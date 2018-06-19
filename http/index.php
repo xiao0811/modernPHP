08b9f632efa315ba15ee22781d52c9ddc8e0d82b
xiao0811 <panxiao0811@gmail.com>
2018-06-19 19:16:16 +0800

    guzzlehttp/guzzle

---
+++ http/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panxiao
+ * Date: 2018/6/19
+ * Time: 下午7:04
+ */
+require "vendor/autoload.php";
+require_once "../common/function.php";
+
+$client = new \GuzzleHttp\Client();
+$res = $client->request('get', 'http://www.baidu.com');
+
+dd($res->getHeaders());
